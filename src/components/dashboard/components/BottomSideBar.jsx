b88b230c7abadcaf5db252c2983b0c45b8d11afa
rasouljabbari <rasoul_jabbari_1377@yahoo.com>
2024-04-08 12:10:34 +0330

    log out handler

--- src/components/dashboard/components/BottomSideBar.jsx
+++ src/components/dashboard/components/BottomSideBar.jsx
@@ -9,6 +9,11 @@ import BottomFixedButton from "../../utils-components/button/BottomFixedButton";
 
 function BottomSideBar() {
 
+  const logoutHandler = () => {
+    localStorage.clear()
+    window.location.href = "/login"
+  }
+
   return (
     <BottomFixedButton>
       {BOTTOM_PAGES.map((name) => (
@@ -25,6 +30,9 @@ function BottomSideBar() {
           </div>
         </NavLink>
       ))}
+      <button onClick={logoutHandler} className="flex-center flex-col gap-y-1 relative">
+        <i className={`icon-log-out-01 text-rose-500 text-2xl`} />
+      </button>
     </BottomFixedButton>
   );
 }
